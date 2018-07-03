91810eb947b00419accad6d349557d46d896eee7
Stefan Buhrmester <buhrmi@gmail.com>
2018-07-03 19:01:57 +0900

    show balances

--- pages/balances/index.vue
+++ pages/balances/index.vue
@@ -2,5 +2,13 @@
   .page
     .wrapper
       h1 Shitcoin Balances
-      p Under construction
+      .balance(v-for="balance in balances")
+        p {{ balance.asset.name }}: {{ balance.balance }}
 </template>
+
+<script lang="coffee">
+module.exports =
+  asyncData: ({app: {$axios}}) ->
+    balances: await $axios.$get('/balances')
+
+</script>
