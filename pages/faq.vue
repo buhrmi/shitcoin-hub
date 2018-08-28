6947a2d87b363e53f8584e65200a5518baadc943
Stefan Buhrmester <buhrmi@gmail.com>
2018-08-28 17:59:13 +0900

    update faq and price display

--- pages/faq.vue
+++ pages/faq.vue
@@ -10,11 +10,22 @@
         .item
           h2 What are Original Shitcoins™?
           p Original Shitcoins™ can be used at the vendors of Shitcoin World™ to buy items and services.
-          p Once per day, a Hodler generates Original Shitcoins™ for its owner based on its current hodlings.
+          p At one point in the future, we will take a snapshot of all Original Shitcoin™ hodlers, and use this snapshot to initialize the genesis state of the ShitChain, which will use Original Shitcoins™ as its native token.
       .container
         .item
-          h2 How is the amount of generated Original Shitcoins™ determined?
-          p The amount is determined by the total amount of Original Shitcoins™ in existence, the current inflation rate, and the current trading price and the trading volume of the Hodler's hodlings. The formula is not finalized yet, but it will be made available here.
+          h2 What type of consensus algorithm will ShitChain use?
+          p PoS (Proof-of-Shit)
+      .container
+        .item
+          h2 How can I get Original Shitcoins™?
+          p There are multiple ways to get Original Shitcoins™:
+          ul
+            li Help your Hodler to fight the FUD that is currently attacking Shitcoin Kingdom
+            li Purchase them on an exchange that supports them
+      .container
+        .item
+          h2 What is the value of an Original Shitcoin™?
+          p ¯\_(ツ)_/¯
       .container
         .item
           h2 Who holds the Shitcoin World™ trade mark?
