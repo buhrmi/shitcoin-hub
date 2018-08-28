606539de980412cad6125a2956584438712c5a3d
Stefan Buhrmester <buhrmi@gmail.com>
2018-08-28 22:34:47 +0900

    more faq tweaks

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
   .page
     .wrapper
+      img(src="~/assets/wtfisthis.png" style="width: 400px")
       h1 FAQ
       p Frequently asked questions
       .container
         .item
           h2 What is Shitcoin World™?
-          p Shitcoin World™ is a persistent universe currently under development by Shitcoin Inc.
+          p Shitcoin World™ is an online game currently under development by Shitcoin Inc.
       .container
         .item
           h2 What are Original Shitcoins™?
           p Original Shitcoins™ are tokens issued on the original Ethereum blockchain (aka Ethereum Classic) that can be used to purchase items and services at the vendors of Shitcoin World™.
+      .container
+        .item
+          h2 What is the value of an Original Shitcoin™?
+          p ¯\_(ツ)_/¯
       .container
         .item
           h2 How can I get Original Shitcoins™?
@@ -21,20 +26,16 @@
             li Ask a friend to send you some
       .container
         .item
-          h2 What is the initial distribution of Original Shitcoins™?
-          p 100% of all Original Shitcoins™ are assigned to be earned by players of the game and will be minted in batches as neccesary. The developers do not issue or reserve any Original Shitcoins™ for themselves.
-      .container
-        .item
           h2 Where can I see how many Original Shitcoins™ have been issued in total?
           p You can always find the current total supply on <a href="https://etherhub.io/token/0x71d6cce7566297c85d6bb6ef6644d1a171efda60" target="_blank">etherhub.io</a> (look for the number next to "Total 💩 Tokens")
       .container
         .item
-          h2 Will I be able to sell a Hodler?
-          p Yes, you will be able to register a Hodler as an NFT and sell it on the <a href="https://opensea.io">Open Sea Marketplace</a>.
+          h2 What is the initial distribution of Original Shitcoins™?
+          p All (that's 100%) Original Shitcoins™ are being distributed in-game through normal gameplay to players and will be minted in batches as it becomes neccesary. The developers do not issue or reserve any Original Shitcoins™ for themselves or other third-parties.
       .container
         .item
-          h2 What is the value of an Original Shitcoin™?
-          p ¯\_(ツ)_/¯
+          h2 Will I be able to sell a Hodler?
+          p Yes, you will be able to register a Hodler as an NFT and sell it on the <a href="https://opensea.io">Open Sea Marketplace</a>.
       .container
         .item
           h2 Who holds the Shitcoin World™ trade mark?
