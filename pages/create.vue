b15d5fa22bc1ba9cb7fdbcd71b7f4131963fa301
Stefan Buhrmester <buhrmi@gmail.com>
2018-09-11 18:00:41 +0900

    more shitcoin maker features

--- pages/create.vue
+++ pages/create.vue
@@ -3,46 +3,63 @@
     .wrapper
       .redactor-styles.no-padding
         h1 Shitcoin Maker™
-        p Shitcoin Maker will become the Shitcoin World ICO Platform
-        form(:action="createUrl")
-          table.form
-            tbody
-              tr.field
-                th.label Name
-                td.input
-                  input(name="name" v-model="newCoin.name")
-              tr.field
-                th.label Symbol
-                td.input
-                  input(name="symbol" v-model="newCoin.symbol")
-              tr.field
-                th.label Decimals
-                td.input
-                  input(name="decimals" v-model="newCoin.decimals")
-              tr.field
-                th.label {{ newCoin.mintable ? 'Initial Supply' : 'Total (max) Supply'}}
-                td.input
-                  input(name="initial_supply" v-model="newCoin.initialSupply")
-                  label
-                    input(type="checkbox" name="mintable" v-model="newCoin.mintable")
-                    |  Enable minting
-                    
-              tr.field(v-if="newCoin.mintable")
-                th.label Max supply
-                td.input
-                  input(name="max_supply" v-model="newCoin.maxSupply")
-              tr.field
-                th.label
-                td.input
-                  label
-                    input(type="checkbox" name="pausable" v-model="newCoin.pausable")
-                    |  Enable pausing
-          button Download Contract
-          p 
-            | After downloading, open the file in  
-            a(target="_blank" href="http://remix.ethereum.org/") Remix
-            |  to deploy the contract to the network.
-
+        p An ICO platform as easy as 1 2 3...
+        .container
+          .card.padded
+            h2 Step 1: Create Token
+            form(:action="createUrl")
+              table.form
+                tbody
+                  tr.field
+                    th.label Name
+                    td.input
+                      input(name="name" v-model="newCoin.name")
+                  tr.field
+                    th.label Symbol
+                    td.input
+                      input(name="symbol" v-model="newCoin.symbol")
+                  tr.field
+                    th.label Decimals
+                    td.input
+                      input(name="decimals" v-model="newCoin.decimals")
+                  tr.field
+                    th.label {{ newCoin.mintable ? 'Initial Supply' : 'Total (max) Supply'}}
+                    td.input
+                      input(name="initial_supply" v-model="newCoin.initialSupply")
+                      label
+                        input(type="checkbox" name="mintable" v-model="newCoin.mintable")
+                        |  Enable minting
+                        
+                  tr.field(v-if="newCoin.mintable")
+                    th.label Max supply
+                    td.input
+                      input(name="max_supply" v-model="newCoin.maxSupply")
+                      |  (0 for unlimited)
+                  tr.field
+                    th.label
+                    td.input
+                      label
+                        input(type="checkbox" name="pausable" v-model="newCoin.pausable")
+                        |  Enable pausing
+              button Download Contract
+          .card.padded
+            h2 Step 2: Deposit Token
+            p 
+              | After downloading, open the file in  
+              a(target="_blank" href="http://remix.ethereum.org/") Remix
+              |  to deploy the contract to the network.
+            .deposit(v-if="$store.state.user")
+              p After you have deployed your contract, deposit some tokens in your personal deposit address.
+              p Your deposit address: {{ $store.state.addresses[1]}}
+              img(:src="`https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=${this.$store.state.addresses[1]}&choe=UTF-8`")
+            .signup(v-else)
+              p You need to sign up or log in to view your deposit address.
+              button(@click="$store.state.showSignup = true") Sign up
+              |  or 
+              a(@click="$store.state.showSignup = true") Log in
+          .card.padded
+            h2 Step 3: Trade Token
+            p Waiting for deposit... Once your deposit has been confirmed, you will be automatically redirected to the trading view.
 </template>
 
 <script lang="coffee">
