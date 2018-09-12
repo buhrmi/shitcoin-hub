6a62ac16a03854d5b0a47490b9307bf6a5e71974
Stefan Buhrmester <buhrmi@gmail.com>
2018-09-12 20:00:49 +0900

    disclaimer

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,7 +27,7 @@
                 td.input
                   label
                     input(type="checkbox" v-model="liablilty_removed")
-                    |  I agree to NEVER invest more than what I can afford to lose, and to not hold Shitcoin Inc or its staff liable for any damages that might occur.
+                    |  I agree to NEVER invest more than what I can afford to lose.
               tr.field
                 td.label
                 td.input
