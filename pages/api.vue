1eee879f5e193fca2d6a880bf9815fd9575704e7
Stefan Buhrmester <buhrmi@gmail.com>
2018-09-12 13:42:26 +0900

    put back api page

---
+++ pages/api.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .page
+    .wrapper
+      h1 API
+      p We are working on a <a target="_blank" href="https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md">binance-compatible</a> API that can be used for automated arbitration with other exchanges.
+      .container
+        .card.padded
+          h2 Orders
+          h3 Create an order
+          p Coming soon
+          h3 Update an order
+          p Coming soon
+          h3 List Orders
+          p Coming soon
+        .card.padded
+          h2 Pricing
+          h3 Get price information
+          p Coming soon
+</template>
+
