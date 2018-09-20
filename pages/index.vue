2aee16097eb1345c9fb1bb9058897b8ed31ec52d
DJ Shitcoin™ <dj@shitcoin.jp>
2018-09-20 15:47:04 +0900

    update landing page

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,18 @@
     .wrapper
       .section.welcome
         h1 Welcome to Shitcoin Kingdom
-        h2 Create a Hodler, join the fight against the FUD and trade Shitcoins!
-        p Total amount of Original Shitcoins™ distributed: {{ $store.state.status.shitcoins_earned }}
-        nuxt-link.button(to="/hodlers/new") Create a Hodler
-        nuxt-link(to="/faq") or learn more about Original Shitcoins™
+        h2 Create a Hodler, join the fight against the FUD and get Shitcoins!
+        .shitcoin_info
+          .half
+            .total Total amount of 💩 distributed
+            .distributed {{ $store.state.status.shitcoins_earned }}
+          .half
+            .total Last Price
+            .distributed 1💩 = {{ $store.state.prices[$store.state.status.original_shitcoin_id] }} {{ $store.state.quote_shitcoin.symbol }}
+        .main_coa
+          nuxt-link.button(to="/hodlers/new") Make your own FREE Hodler
+        | or 
+        nuxt-link(to="/faq") learn more about Original Shitcoins™
       .section
         h1 Say hello to new Hodlers
         .container
@@ -38,14 +46,34 @@ module.exports =
 .section {
   margin-bottom: 100px;
 }
+.shitcoin_info {
+  margin: 80px 0;
+  .half {
+    display: inline-block;
+    width: 50%;
+  }
+}
+.main_coa {
+  .button {
+    padding: 19px 30px;
+    font-size: 27px;
+  }
+}
 .welcome {
+  text-align: center;
   h1 {
-    font-size: 40px;
-    margin: 30px 0;
+    font-size: 50px;
+    margin: 40px 0;
   }
   h2 {
-    margin: 20px 0;
+    margin: 30px 0;
   }
-  text-align: center;
+  .total {
+    font-size: 22px;
+  }
+  .distributed {
+    font-weight: bold;
+    font-size: 40px;
+  }
 }
 </style>
