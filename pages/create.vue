e945012c5f50fbb3036e9929352333dbcd01133d
DJ Shitcoin™ <dj@shitcoin.jp>
2018-09-21 11:26:39 +0900

    add support for crowdsale

--- pages/create.vue
+++ pages/create.vue
@@ -41,6 +41,44 @@
                       label
                         input(type="checkbox" name="pausable" v-model="newCoin.pausable")
                         |  Enable pausing
+                  tr.field(v-if="newCoin.mintable")
+                    th.label
+                    td.input
+                      label
+                        input(type="checkbox" name="crowdsale" v-model="newCoin.crowdsale")
+                        |  Enable Crowd Sales
+                  tr.field(v-if="newCoin.crowdsale && newCoin.mintable")
+                    th.label Start Date
+                    td.input
+                      input(name="start_date" type="datetime-local" v-model="newCoin.start_date")
+                      |  UTC (GMT+0)
+                  tr.field(v-if="newCoin.crowdsale && newCoin.mintable")
+                    th.label Price
+                    td.input
+                      | 1 {{ newCoin.symbol }} = 
+                      input(name="price" v-model="newCoin.price")
+                      |  ETH
+                  tr.field(v-for="(phase, index) in newCoin.phases" v-if="newCoin.crowdsale && newCoin.mintable")
+                    th.label
+                      | Phase {{ index + 1}} 
+                      a(@click="newCoin.phases.splice(index, 1)") Remove phase
+                    td.input
+                      .bonus
+                        | % Bonus: 
+                        input(:name="`phases[]bonus`" v-model="phase.bonus")
+                      .end
+                        | End: 
+                        input(:name="`phases[]end`" type="datetime-local" v-model="phase.end")
+                  tr.field(v-if="newCoin.crowdsale && newCoin.mintable")
+                    th.label
+                    td.input
+                      label
+                        button(@click.prevent="addphase") Add Phase
+                  tr.field(v-if="newCoin.crowdsale && newCoin.mintable")
+                    th.label End Date
+                    td.input
+                      input(name="end_date" type="datetime-local" v-model="newCoin.end_date")
+                      |  UTC (GMT+0)
               button Download Contract
         .container
           .card.padded
@@ -84,7 +122,14 @@ module.exports =
         maxSupply: '20000000'
         address: ''
         platform_id: 1
+        crowdsale: false
+        start_date: new Date()
+        end_date: new Date()
+        price: 1
+        phases: []
   methods:
+    addphase: ->
+      this.newCoin.phases.push bonus: 0, start: new Date()
     createShitcoin: ->
       newShitcoin = await this.$axios.$post('/shitcoins', {shitcoin: this.newCoin})
       this.$router.push(name: 'shitcoins-id-funding', params: {id: newShitcoin.param})
